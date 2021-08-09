c86f5d9a5ca844bf238a9f87c60639476293a32a
Cassandra <cassandraasdanielson@gmail.com>
2021-08-09 13:53:19 -0500

    script.js at 80% complete

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 var block = document.getElementById("block");
 var hole = document.getElementById("hole");
+var character = document.getElementById("character");
+var jumping = 0;
 
 hole.addEventListener('animationiteration', () => {
 	var random = -((Math.random()*300)+150);
 	hole.style.top = random + "px"
 });
+
+setInterval(function(){
+	var characterTop = 
+	parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+	if(jumping==0) {
+		character.style.top = (characterTop+3)+"px";
+	}
+},10);
+
+function jump() {
+	jumping = 1;
+}
